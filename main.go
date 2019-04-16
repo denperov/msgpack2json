d5eefc2ffd83b8c2514682d55325704d655217b8
Denis Perov <denis.perov@aviasales.ru>
2019-04-16 20:17:37 +0700

    Merge arrays option

--- main.go
+++ main.go
@@ -10,31 +10,74 @@ import (
 )
 
 var indentation bool
+var mergeArrays bool
 
 func init() {
 	flag.BoolVar(&indentation, "i", false, "indentation")
+	flag.BoolVar(&mergeArrays, "m", false, "merge-arrays")
 }
 
 func main() {
 	flag.Parse()
 
-	decoder := msgpack.NewDecoder(os.Stdin)
- 	encoder := json.NewEncoder(os.Stdout)
- 	if indentation {
+	reader := os.Stdin
+	writer := os.Stdout
+
+	decoder := msgpack.NewDecoder(reader)
+	encoder := json.NewEncoder(writer)
+	if indentation {
 		encoder.SetIndent("", "\t")
 	}
 
-	for {
-		var content interface{}
-		err := decoder.Decode(&content)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
+	if !mergeArrays {
+		for {
+			var content interface{}
+			err := decoder.Decode(&content)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
 
-		err = encoder.Encode(content)
+			err = encoder.Encode(content)
+			if err != nil {
+				panic(err)
+			}
+		}
+	} else {
+		begin := true
+		for {
+			var content []interface{}
+			err := decoder.Decode(&content)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+
+			for _, item := range content {
+				if begin {
+					begin = false
+					_, err := writer.Write([]byte{'{'})
+					if err != nil {
+						panic(err)
+					}
+				} else {
+					_, err := writer.Write([]byte{','})
+					if err != nil {
+						panic(err)
+					}
+				}
+
+				err := encoder.Encode(item)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+		_, err := writer.Write([]byte{']'})
 		if err != nil {
 			panic(err)
 		}
